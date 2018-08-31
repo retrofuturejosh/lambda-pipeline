580bb6af687a84abc53d0a1cc77c5136e3871b31
retrofuturejosh <joshuadsohn@gmail.com>
2018-08-31 16:24:15 -0400

    initial commit

---
+++ scripts/create-stacks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#create CodeBuild resources
+echo creating codebuild resources
+aws cloudformation create-stack --stack-name serverless-pipeline-resources --template-body file://templates/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters file://templates/params.json
+
+#wait for resources to be created
+echo waiting for successful creation of stack
+aws cloudformation wait stack-create-complete --stack-name serverless-pipeline-resources
+
+echo successfully created stack
