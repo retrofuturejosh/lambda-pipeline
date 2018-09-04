fc5c78ae0bd2a43b0b641dad4fc49c5a4dfa00c4
retrofuturejosh <joshuadsohn@gmail.com>
2018-09-04 16:47:20 -0400

    [edits] pre-traffic hook code

--- src/preTrafficHook.js
+++ src/preTrafficHook.js
@@ -1,47 +1,59 @@
 'use strict';
 const aws = require('aws-sdk');
-const codedeploy = new aws.CodeDeploy({apiVersion: '2014-10-06'});
+const codedeploy = new aws.CodeDeploy({ apiVersion: '2014-10-06' });
+const lambda = new aws.Lambda();
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context) => {
+  console.log('Entering PreTraffic Hook!');
+  console.log('Event is \n\n', JSON.stringify(event));
 
-	console.log("Entering PreTraffic Hook!");
-	console.log(JSON.stringify(event));
+  //Read the DeploymentId from the event payload.
+  const deploymentId = event.DeploymentId;
 
-	//Read the DeploymentId from the event payload.
-    const deploymentId = event.DeploymentId;
-	console.log(`deploymentId is \n\n`, deploymentId);
+  //Read the LifecycleEventHookExecutionId from the event payload
+  const lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
 
-    //Read the LifecycleEventHookExecutionId from the event payload
-	const lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
-	console.log(`lifecycleEventHookExecutionId is \n\n`, lifecycleEventHookExecutionId);
+  //get ARN of lambda function from environment
+  const lambdaVersion = process.env.CURRENT_VERSION;
+  console.log('lambdaVersion is \n\n', lambdaVersion);
 
-	const lambdaVersion = process.env.CURRENT_VERSION;
-	console.log('lambdaVersion is \n\n', lambdaVersion);
+  // Prepare the validation test results with the deploymentId and
+  // the lifecycleEventHookExecutionId for AWS CodeDeploy.
+  let codedeployParams = {
+    deploymentId: deploymentId,
+    lifecycleEventHookExecutionId: lifecycleEventHookExecutionId,
+    status: 'Failed' // status can be 'Succeeded' or 'Failed'
+  };
 
-	/*
-		[Perform validation or prewarming steps here]
-	*/
+  const lambdaParams = {
+    FunctionName: lambdaVersion /* required */
+  };
 
-	// Prepare the validation test results with the deploymentId and
-    // the lifecycleEventHookExecutionId for AWS CodeDeploy.
-    const params = {
-        deploymentId: deploymentId,
-        lifecycleEventHookExecutionId: lifecycleEventHookExecutionId,
-        status: 'Succeeded' // status can be 'Succeeded' or 'Failed'
-    };
+  let lambdaResponse = await lambda
+    .invoke(lambdaParams)
+    .promise()
+    .then(res => {
+      return JSON.parse(res.Payload);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
-	// Pass AWS CodeDeploy the prepared validation test results.
-    codedeploy.putLifecycleEventHookExecutionStatus(params, function(err, data) {
-        if (err) {
-			// Validation failed.
-			console.log('Validation test failed');
-			console.log(err);
-			console.log(data);
-            callback('Validation test failed');
-        } else {
-			// Validation succeeded.
-			console.log('Validation test succeeded');
-            callback(null, 'Validation test succeeded');
-        }
-	});
-}
+  if (lambdaResponse.statusCode === '200') {
+    codedeployParams.status = 'Succeeded';
+    console.log('Tests Passed');
+  } else {
+    console.log('Tests Failed');
+  }
+
+  // Pass AWS CodeDeploy the prepared validation test results.
+  return await codedeploy
+    .putLifecycleEventHookExecutionStatus(codedeployParams)
+    .promise()
+    .then(success => {
+      return success;
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+};
