428cd43e24cde653b6d5fe531f2224ffa080c9bb
retrofuturejosh <joshuadsohn@gmail.com>
2018-09-13 13:24:22 -0400

    edits pipeline

--- scripts/create-stacks.sh
+++ scripts/create-stacks.sh
@@ -2,7 +2,7 @@
 
 #create CodePipeline resources
 echo creating pipeline resources
-aws cloudformation create-stack --stack-name serverless-pipeline-resources --template-body file://templates/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters file://templates/params.json
+aws cloudformation create-stack --stack-name serverless-pipeline-resources --template-body file://pipeline/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters file://pipeline/params.json
 
 #wait for resources to be created
 echo waiting for successful creation of stack
