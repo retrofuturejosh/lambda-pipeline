133775edc5617690959f9022f617351650b7e8d2
retrofuturejosh <joshuadsohn@gmail.com>
2018-09-18 15:14:17 -0400

    debugging

--- scripts/test-deployment.sh
+++ scripts/test-deployment.sh
@@ -2,7 +2,7 @@
 
 result=0
 
-for i in {1..40}; do NODE_ENV=integration mocha 'test/integration/api' --recursive;if [ $? -ne 0 ]; then result=1; fi; sleep 15; done;
+for i in {1..40}; do NODE_ENV=integration mocha 'test/integration/api' --recursive; if [ $? -ne 0 ]; then result=1; else result=0; fi; sleep 15; done;
 
 echo $result
 
