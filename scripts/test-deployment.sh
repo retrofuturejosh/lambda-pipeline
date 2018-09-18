c89b6fbcce502de97c6428f6bb0f1f870ca30644
retrofuturejosh <joshuadsohn@gmail.com>
2018-09-18 15:36:12 -0400

    [cleans] test-deployment bash script

--- scripts/test-deployment.sh
+++ scripts/test-deployment.sh
@@ -2,7 +2,19 @@
 
 result=0
 
-for i in {1..40}; do NODE_ENV=integration mocha 'test/integration/api' --recursive; if [ $? -ne 0 ]; then result=1; else result=0; fi; sleep 15; done;
+sleep 60
+
+# test API every 15 seconds for 10 minutes
+for i in {1..40}; do NODE_ENV=integration mocha 'test/integration/api' --recursive
+# check for errors and assign result
+if [ $? -eq 0 ]
+then result=0
+else result=1
+fi
+# if tests fail more than 5 times, fail out
+if [[ $result -eq 1 && i -gt 5 ]]
+then exit $result
+fi; sleep 15; done;
 
 echo $result
 
